5eb07331e1e36550974d6bce1c5df281e8f30454
matt769 <matthedley@hotmail.co.uk>
2017-11-09 03:51:03 +0000

    python data parsing needs some work. added python function updatePositionAfterMovement and calculateSensorReadingPositions

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -25,9 +25,26 @@ except serial.SerialException:
 	print "Exiting program"
 	exit()
 
+	
+
+######################################
+# MAIN SETUP #########################
+######################################
+
+globalPosition = (0.0, 0.0)
+globalHeading = 0.0
+angleTurned = 0
+distanceMoved = 0
+readingAngles = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170]
+# modify parser to extract the motion information
+
+######################################
+# FUNCTIONS# #########################
+######################################
+
 # for sending
 lastSent = time.clock()
-sendFreq = 1	# in seconds
+sendFreq = 5	# in seconds
 def send():
 	global lastSent
 	if time.clock() - lastSent > sendFreq:
@@ -63,15 +80,72 @@ def parseData(str):
 		return False
 	return True, inputArrayNum
 
+# don't think this is the best solution but go with it for now
+# prob change to return some data instead of modifying globals ??
+def parseData2(str):
+	global angleTurned
+	global distanceMoved
+	inputArray = str.split()
+	inputArrayNum = []
+	packetType = 0
+	#print inputArray
+	try:
+		if int(inputArray[0]) == 1:	# motion packet
+			packetType = 1
+			angleTurned = float(inputArray[3])
+			distanceMoved = float(inputArray[4])
+		elif int(inputArray[0]) == 2:	# sensor packet
+			packetType = 2
+			for i in inputArray:
+				#print inputArray[i]
+				#tmp = int(inputArray[i])
+				#print tmp
+				inputArrayNum.append(int(i))
+	except:
+		print "conversion failed"
+		return 0
+	return packetType, inputArrayNum
 
+def updatePositionAfterMovement(angleTurned, distanceMoved):
+	globalHeading = (globalHeading + angleTurned) % 360
+	globalPosition[0] = globalPosition[0] + distanceMoved * sin(globalHeading)	# CHECK THIS
+	globalPosition[1] = globalPosition[1] + distanceMoved * cos(globalHeading)	# CHECK THIS
+
+
+def calculateSensorReadingPositions(angles, distances):
+	readingPositions = []
+	for idx in distances:#
+		# add sense check
+		x = globalPosition[0] + distances[idx] * sin(angles[idx])	# CHECK THIS
+		y = globalPosition[1] + distances[idx] * cos(angles[idx])	# CHECK THIS
+		readingPositions.append((x,y))
+
+
+
+globalPosition = (0.0, 0.0)
+globalHeading = 0.0
+angleTurned = 0
+distanceMoved = 0
+
+
+
+
+
+######################################
+# MAIN START #########################
+######################################
 while True:
 	send()
 	(result,input) = listenAndReceive(1)
 	#print result, input
 	if result:
 		(result, data) = parseData(input)
-	if result:
+	if result > 0:
 		print data
+
+######################################
+# MAIN END ###########################
+######################################
 
 
 # WHAT's THE FLOW?
@@ -80,7 +154,6 @@ while True:
 #do some calculations
 # repeat
 # (in future may get multiple data receipts before deciding to take action)
-
 # will need to understand when to stop receiving
 
 
